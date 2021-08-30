1f90b942d0d23c084ef02ff178eeef08110d631a
parmindersingh4385 <singh1.parminder@gmail.com>
2021-08-30 23:34:37 +0530

    Commit changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path')
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+//const path = require('path')
 const app = express();
 //require('./database');
 
-app.use(bodyParser.json());
-app.use(cors());
+/* app.use(bodyParser.json());
+app.use(cors()); */
 
 // API
 /* const users = require('/api/users');
